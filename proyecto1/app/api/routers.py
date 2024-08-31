900990dd44b3f302cb6d3c85bd07f0b66fda3184
PaulaAndreaO05 <paula.alvarez@correo.tdea.edu.co>
2024-08-31 10:33:15 -0500

    segundo cambio

---
+++ proyecto1/app/api/routers.py
@@ -0,0 +1,8 @@
+# from rest_framework.routers import DefaultRouter
+# from ..state.views import *
+
+# router = DefaultRouter()
+
+# router.register(r'state', StateViewset, basename='state')
+
+# urlpatterns = router.urls
