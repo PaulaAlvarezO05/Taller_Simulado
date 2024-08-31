e2a04ef9dd76c5613ce7c961a6aef08e79208032
PaulaAndreaO05 <paula.alvarez@correo.tdea.edu.co>
2024-08-31 11:37:30 -0500

    paula2

--- proyecto1/app/libro/models.py
+++ proyecto1/app/libro/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ..autor.models import Autor 
+from ..genero.models import Genero
 
 # Create your models here.
+class Libro (models.Model):
+    titulo = models.CharField("Titulo", max_length=200)  
+  
+    idAutor = models.ForeignKey ( 
+    Autor, 
+    verbose_name="Autor", 
+    on_delete=models.CASCADE, 
+    null=True, 
+    blank=True 
+    ) 
+
+    fecha =models.DateTimeField("Fecha de publicación", max_length=50)
+
+    idGenero = models.ForeignKey ( 
+    Autor, 
+    verbose_name="Genero", 
+    on_delete=models.CASCADE, 
+    null=True, 
+    blank=True 
+    ) 
+    disponibilidad= models.BooleanField(default=False)
+    
+
+
+
