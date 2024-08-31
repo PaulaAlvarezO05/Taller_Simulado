6c9fb019ecbc8cf4dbbb55404dc7a94ea21678a7
YulexaAragon <yulearagon@gmail.com>
2024-08-31 11:36:32 -0500

    Yulexa2

--- proyecto1/app/autor/models.py
+++ proyecto1/app/autor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    class Meta: 
+        verbose_name = "Autor" 
+        verbose_name_plural = "Autores" 
+    
+    nombre = models.CharField("Nombre", max_length=100)
--- proyecto1/app/genero/models.py
+++ proyecto1/app/genero/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Autor(models.Model):
-    class Meta: 
-        verbose_name = "Autor" 
-        verbose_name_plural = "Autores" 
-    
-    nombre = models.CharField("Nombre", max_length=100)
 
  
